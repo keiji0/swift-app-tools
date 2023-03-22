127db4c6aeb81b4422ad1365e0e44918f8d4b42a
keiji0 <keiji0@gmail.com>
2023-03-22 23:09:55 +0900

    Boxタイプを追加

---
+++ Sources/AppTools/Data/Box.swift
@@ -0,0 +1,14 @@
+//
+//  Box.swift
+//  
+//  
+//  Created by keiji0 on 2023/03/22
+//  
+//
+
+public final class Box<Value> {
+    public var value: Value
+    public init(_ value: Value) {
+        self.value = value
+    }
+}
