887330e7111efcb50b2eadc982a9273456fc0081
keiji0 <keiji0@gmail.com>
2023-03-25 22:28:28 +0900

    配列から指定要素を削除するメソッドを追加

---
+++ Sources/AppTools/Extension/Array.swift
@@ -0,0 +1,19 @@
+//
+//  Array.swift
+//  
+//  
+//  Created by keiji0 on 2023/03/25
+//  
+//
+
+import Foundation
+
+extension Array {
+    /// 指定要素を削除する
+    public mutating func removeFirst(of element: Element) where Element: Equatable {
+        guard let index = firstIndex(of: element) else {
+            return
+        }
+        removeFirst(index)
+    }
+}
